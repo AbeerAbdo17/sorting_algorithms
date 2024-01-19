72a883b0a8c5b217d546b67743b152c7cd489452
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-01-19 01:17:16 -0800

    tes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,40 +3,41 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list using Insertion sort
- * @list: Pointer
+ * @list: Pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *xcurr, *xtmp;
-
-	if (list || *list || (*list)->next)
+	if (list != NULL && *list != NULL && (*list)->next != NULL)
 	{
+		listint_t *xcurr, *xtmp, *temp;
 
 		xcurr = (*list)->next;
 
 		while (xcurr != NULL)
 		{
 			xtmp = xcurr;
+			xcurr = xcurr->next;
 
 			while (xtmp->prev != NULL && xtmp->n < xtmp->prev->n)
 			{
+				temp = xtmp->prev;
+
 				if (xtmp->next != NULL)
-					xtmp->next->prev = xtmp->prev;
-				xtmp->prev->next = xtmp->next;
-				xtmp->next = xtmp->prev;
-				xtmp->prev = xtmp->prev->prev;
-				xtmp->next->prev = xtmp;
+					xtmp->next->prev = temp;
+
+				temp->next = xtmp->next;
+				xtmp->prev = temp->prev;
+				temp->prev = xtmp;
 
 				if (xtmp->prev != NULL)
 					xtmp->prev->next = xtmp;
 				else
 					*list = xtmp;
 
-				print_list(*list);
-			}
+				xtmp->next = temp;
 
-			xcurr = xcurr->next;
+				print_list((const listint_t *)*list);
+			}
 		}
 	}
 }
-
